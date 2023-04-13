06875b1e8673757271aee2fe80ffd2870ecc24ae
Preston Watts <pwatts2132@gmail.com>
2023-04-12 22:34:55 -0500

    got basic outline & styling complete

---
+++ index.js
@@ -0,0 +1,60 @@
+const UNIT_SIZE = 25;
+const gameBoard = document.querySelector('.game-canvas');
+const gameScore = document.querySelector('.game-score');
+const resetButton = document.querySelector('.reset-btn');
+const foodColor = "red";
+const snakeColor = "green";
+const ctx = gameBoard.getContext('2d');
+const borderColor = "black";
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+let = xVelocity = UNIT_SIZE;
+let = yVelocity = 0;
+let gameRunning = false;
+let snake = [{x: UNIT_SIZE*5, y: UNIT_SIZE*5}];
+window.addEventListener("keydown", changeDirection);
+resetButton.addEventListener("click", resetGame);
+
+const gameStart = () => {
+
+}
+
+const nextTick = () => {
+
+}
+
+const clearBoard = () => {
+
+}
+
+const createFood = () => {
+
+}
+
+const drawFood = () => {
+
+}
+
+const moveSnake = () => {
+
+}
+
+const drawSnake = () => {
+
+}
+
+const changeDirection = () => {
+
+}
+
+const checkGameOver = () => {
+
+}
+
+const displayGameOver = () => [
+
+]
+
+const resetGame = () => {
+
+}
